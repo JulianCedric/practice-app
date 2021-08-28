011449ae80bb19a6d936f7a0e06e59b05601329b
JulianCedric <julian.pormentilla@gmail.com>
2021-08-28 16:02:14 -0400

    JSAlgos

---
+++ src/JSAlgo2.js
@@ -0,0 +1,33 @@
+// 2021.06.28 - MON:
+
+// Regex
+
+let username = "JackOfAllTrades";
+let userCheck = /^[a-z]/; // Change this line
+let result = userCheck.test(username);
+
+console.log("result:", result);
+
+// 6/13 passing:
+
+let username = "JackOfAllTrades";
+let userCheck = /^[a-z]/; // Change this line
+let result = userCheck.test(username);
+
+console.log("result:", result);
+
+// 8/13:
+
+let username = "JackOfAllTrades";
+let userCheck = /^[a-z]/i; // Change this line
+let result = userCheck.test(username);
+
+console.log("result:", result);
+
+// 13/13 passing!
+
+let username = "JackOfAllTrades";
+let userCheck = /^[a-z]([a-z]+\d*|[\d][\d])$/i; // Change this line
+let result = userCheck.test(username);
+
+console.log("result:", result); 
