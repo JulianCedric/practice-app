724c9d885210a34c39b0bb8867212eecaaa34bde
JulianCedric <julian.pormentilla@gmail.com>
2021-09-08 15:51:16 -0400

    Fair Candy Swap - LeetCode

--- src/LeetCode4.js
+++ src/LeetCode4.js
@@ -1,3 +1,68 @@
+// 2021.09.08 - WED: 
+
+// Fair Candy Swap
+
+var alice = [1,1];
+var bob = [2,2];
+var alice2 = [1,2];
+var bob2 = [2,3];
+
+function fairCandySwap(A, B) {
+  console.log("A:", A);
+  console.log("B:", B);
+
+  let output = [];
+  // let output = new Array(2);
+  let mapB = {};
+  let totalA = 0;
+  let totalB = 0;
+  
+  for (let i=0; i<A.length; i++) {
+    totalA += A[i];
+  }
+  console.log("totalA:", totalA);
+
+  for (let i=0; i<B.length; i++) {
+    totalB += B[i];
+    mapB[B[i]] = true; // mapB[2] = true
+  }
+  console.log("totalB:", totalB);
+  console.log("mapB:", mapB);
+
+  let delta = ((totalB - totalA) / 2);
+  console.log("delta:", delta);
+
+  for (let i=0; i<A.length; i++) {
+    if (mapB[A[i] + delta]) {
+      output[0] = A[i];
+      output[1] = A[i] + delta;
+      console.log("output:", output);
+      return output;
+    }
+  }
+}
+
+/*
+1. Create output array.
+2. Create mapB object.
+3. Create TotalA variable.
+4. Create TotalB variable.
+5. Loop through A.
+  a. Add values to TotalA.
+6. Loop through B.
+  a. Add values to TotalB.
+  b. Add values to mapB.
+7. Create delta variable equal to (TotalB - TotalA) / 2. 
+8. Loop through A.
+  a. Find if the delta value + current A value exists in B.
+    i. Add the first index value of output to "i".
+    ii. Add the value from the mapB to second index value.
+    iii. Return output.
+*/
+
+// console.log("fairCandySwap(alice, bob):", fairCandySwap(alice, bob));
+console.log("fairCandySwap(alice, bob):", fairCandySwap(alice2, bob2));
+
 // 2021.09.01 - WED: 
 
 var array = [7,1,3,2,4,5,6];
