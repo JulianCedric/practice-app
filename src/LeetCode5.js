c7660e9ca997ade510b3929cb33d661e3ce47fcb
JulianCedric <julian.pormentilla@gmail.com>
2021-10-11 15:50:41 -0400

    LeetCode Practice

---
+++ src/LeetCode5.js
@@ -0,0 +1,38 @@
+// 2021.10.11: 
+    // Started on 10/03
+    // Completed on 10/11
+
+let moves1 = "UD";
+let moves2 = "LL";
+let moves3 = "RRDD";
+let moves4 = "LDRRLRUULR";
+
+function judgeCircle(moves) {
+  let leftOrRight = 0;
+  let upOrDown = 0;
+
+  for (let i=0; i<moves.length; i++) {
+    switch(moves[i]) {
+      case "L": leftOrRight++;
+      break;
+      case "R": leftOrRight--;
+      break;
+      case "U": upOrDown++;
+      break;
+      case "D": upOrDown--;
+    }
+  }
+  console.log("leftOrRight:", leftOrRight);
+  console.log("upOrDown:", upOrDown);
+
+  if (leftOrRight === 0 && upOrDown === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// console.log("judgeCircle(move1):", judgeCircle(moves1));
+// console.log("judgeCircle(move2):", judgeCircle(moves2));
+// console.log("judgeCircle(move3):", judgeCircle(moves3));
+console.log("judgeCircle(move4):", judgeCircle(moves4));
