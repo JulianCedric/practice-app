82beca94ba956bd484c1f08d49f9186102529cc0
JulianCedric <julian.pormentilla@gmail.com>
2021-10-20 14:23:16 -0400

    Student Attendance Record - LeetCode

--- src/LeetCode5.js
+++ src/LeetCode5.js
@@ -1,3 +1,44 @@
+// 2021.10.20:
+
+let str = "PPALLP";
+let str2 = "PPALLL";
+let str3 = "LPLPLPLPLPL";
+let str4 = "LLLALL";
+
+function checkRecord(s) {
+  let a = 0;
+  let consecL = 0;
+
+  for (let i=0; i<s.length; i++) {
+    if (s[i] === "A") {
+      a++;
+    }
+    if (s[i] === "L") {
+      // If current index value is "L", then increment consecL.
+      consecL++;
+      if (consecL >= 3) {
+        return false;
+      }
+    } else {
+      consecL = 0;
+    } 
+      // If current index value is NOT "L", then set consecL equal to 0.
+  }
+  console.log("a:", a);
+  console.log("consecL:", consecL);
+
+  if (a > 1 || consecL >= 3) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+// console.log("checkRecord(str):", checkRecord(str));
+// console.log("checkRecord(str2):", checkRecord(str2));
+// console.log("checkRecord(str3):", checkRecord(str3));
+console.log("checkRecord(str4):", checkRecord(str4));
+
 // 2021.10.16: Revisited:
 
 let array = [3,8,9,4,4,1];
